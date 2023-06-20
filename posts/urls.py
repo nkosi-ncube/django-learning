c43a12118cbf2e4c97ae7bef6ebc7092f727d8d4
nkosi ncube <nkosi9976@gmail.com>
2023-06-20 08:58:43 +0200

    added crud operations

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
-from .views import BlogView,BlogDetailView
+from .views import(
+     BlogView,
+     BlogDetailView,
+     BlogCreateView,
+     BlogUpdateView,
+     BlogDeleteView)
 #pk  is the primary key  as integer -djngo automatically adds incrementing key to our databse models (it adds id)
 urlpatterns=[
     path('',BlogView.as_view(),name='home'),
     path('post/<int:pk>',BlogDetailView.as_view(),name='post_detail'),
+    path('post/new/',BlogCreateView.as_view(),name='post_new'),
+    path('post/<int:pk>/edit',BlogUpdateView.as_view(),name='post_edit'),
+    path('post/<int:pk>/delete',BlogDeleteView.as_view(),name='post_delete')
 ]
