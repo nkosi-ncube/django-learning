ad90432936274f1eac812d7bf6b46f6fb96299c4
nkosi ncube <nkosi9976@gmail.com>
2023-06-23 15:48:31 +0200

    initial

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import BlogHomeView,BlogDetailView
+from .views import BlogHomeView,BlogDetailView,BlogNewPostView,BlogUpdateView,BlogDeleteView
 
 
 urlpatterns=[
     path('',BlogHomeView.as_view(),name='home'),
     path('post_details/<int:pk>',BlogDetailView.as_view(),name='post_detail'),
+    path('post/new/',BlogNewPostView.as_view(),name='post_new'),
+    path('post/<int:pk>/edit/',BlogUpdateView.as_view(),name='post_edit'),
+    path('post/<int:pk>/delete/',BlogDeleteView.as_view(),name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,DeleteView,UpdateView
 from django.views.generic import ListView,DetailView
 from .models import Post
+from django.urls import reverse_lazy
 # Create your views here.
 class BlogHomeView(ListView):
     model = Post
@@ -10,3 +11,20 @@ class BlogHomeView(ListView):
 class BlogDetailView(DetailView):
     model=Post
     template_name='post_detail.html'   
+
+
+#Below are the CRUD operations -create,read,update and delete
+class BlogNewPostView(CreateView):
+    model=Post
+    template_name='post_new.html'
+    fields=['title','author','body']
+    
+class BlogUpdateView(UpdateView):
+    model=Post
+    template_name='post_edit.html'
+    fields=['title','body',]    
+
+class BlogDeleteView(DeleteView):
+    model=Post
+    template_name='post_delete.html'
+    success_url=reverse_lazy('home')    
